bf312cd89295390575d901df57714b56f606f93c
T-107 <T-107@PC242>
2015-10-12 19:04:22 -0500

    new

--- JavaApplication2/src/Collection/Direccion.java
+++ JavaApplication2/src/Collection/Direccion.java
@@ -10,8 +10,41 @@ package Collection;
  * @author T-107
  */
 public class Direccion {
-    String calle;
-    long cp;
-    String colonia
+    private String calle;
+    private long cp;
+    private String colonia;
+    private String municipio;
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public long getCp() {
+        return cp;
+    }
+
+    public void setCp(long cp) {
+        this.cp = cp;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
     
 }
--- JavaApplication2/src/Collection/Usuario.java
+++ JavaApplication2/src/Collection/Usuario.java
@@ -10,5 +10,51 @@ package Collection;
  * @author T-107
  */
 public class Usuario {
+    private String login;
+    private String password;
+    private Direccion direccion;
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+    
     
 }
